6e75ddb5c54805a6c302e129812802e8c7df6c12
apsistec <doug@apsistec.app>
2019-06-25 10:17:57 -0500

    stripe is working correctly

--- src/app/subscription-page/subscription-page.component.ts
+++ src/app/subscription-page/subscription-page.component.ts
@@ -13,10 +13,11 @@ export class SubscriptionPageComponent implements OnInit {
 
 
   constructor(private auth: AuthService, private functions: AngularFireFunctions) { }
-  @Input() plan: string;
- 
+  
+  
   @ViewChild('cardElement') cardElement: ElementRef;
-
+  price = '';
+  planId: string = '';
   stripe: stripe.Stripe;
   card;
   cardErrors;
@@ -56,17 +57,33 @@ export class SubscriptionPageComponent implements OnInit {
     if (error) {
       // Inform the customer that there was an error.
       const cardErrors = error.message;
+      alert(cardErrors);
     } else {
       // Send the token to your server.
       this.loading = true;
       const user = await this.auth.getUser();
       const fun = this.functions.httpsCallable('stripeCreateSubscription');
-      this.confirmation = await fun({ source: source.id, uid: user.uid, plan: this.plan }).toPromise();
+      this.confirmation = await fun({ source: source.id, uid: user.uid, plan: this.planId }).toPromise();
       this.loading = false;
 
     }
+    
+
   }
 
-
-
+  clickedGold() {
+    this.planId = 'Gold';
+    this.price = '$90.00 per year';
+  }
+  
+  clickedSilver() {
+    this.planId = 'Silver';
+    this.price = '$50.00 per 6 months';
+    
+  }
+  
+  clickedBronze() {
+    this.planId = 'Bronze';
+    this.price = '$10.00 per month';
+  }
 }
