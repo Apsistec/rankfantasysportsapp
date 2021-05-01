d81d2acb42dcff12149eb28fb063e081dbec11dd
Doug White <doug@apsistec.app>
2021-05-01 18:09:48 -0500

    updatte

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyADaGJqcu9yjLtHIx2H10Tc3SxSeRAfpSI",
-    authDomain: "rfs-winning.firebaseapp.com",
-    databaseURL: "https://rfs-winning.firebaseio.com",
-    projectId: "rfs-winning",
-    storageBucket: "rfs-winning.appspot.com",
-    messagingSenderId: "186825783910",
-    appId: "1:186825783910:web:de9e8e42ad7bfe565f0ba7",
-    measurementId: "G-0P8VW87DF2"
+    apiKey: "AIzaSyCCDgm1uoUnKNmeUeck_vRwkI8wbiRrmvY",
+    authDomain: "rankfantasysports-app.firebaseapp.com",
+    projectId: "rankfantasysports-app",
+    storageBucket: "rankfantasysports-app.appspot.com",
+    messagingSenderId: "730928216570",
+    appId: "1:730928216570:web:90e0550a7f668655e74c43",
+    measurementId: "G-815BG0Z4DT"
   },
   // tableUrl:
     // 'https://graph.microsoft.com/v1.0/me/drive/items/E6DAB4F84D6D7119!748/workbook/worksheets(\'Thru 01.20.20\')/usedRange?$format=json'
